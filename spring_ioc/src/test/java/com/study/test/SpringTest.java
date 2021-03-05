5b7eff34bad9cc43e4b84958864cdd0fba27a058
Leyusf <53003567+Leyusf@users.noreply.github.com>
2021-03-06 00:15:13 +0800

    2021/3/6 0:14

--- spring_ioc/src/test/java/com/study/test/SpringTest.java
+++ spring_ioc/src/test/java/com/study/test/SpringTest.java
@@ -8,22 +8,33 @@ import org.junit.Test;
 import java.lang.annotation.Target;
 
 public class SpringTest {
-    private ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
     @Test
     //测试scope属性
     //singleton对象在容器创建时创建,只要容器在对象一直活着，应用卸载，容器销毁时对象被销毁
-    //prototype对象在被getBean时创建
+    //prototype对象在被getBean时创建,只要使用就一直存在,长时间不使用就会被回收
     public void test1(){
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDaoImpl userDao1 = (UserDaoImpl) app.getBean("userDao");
         UserDaoImpl userDao2 = (UserDaoImpl) app.getBean("userDao");
         System.out.println(userDao1==userDao2);
     }
 
     @Test
-    //测试对象创建的时机
+    //测试对象创建和销毁时机的时机
     public void test2(){
-        UserDaoImpl userDao1 = (UserDaoImpl) app.getBean("userDao");
-        UserDaoImpl userDao2 = (UserDaoImpl) app.getBean("userDao");
-        System.out.println(userDao1==userDao2);
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDaoImpl userDao = (UserDaoImpl) app.getBean("userDao");
+        ((ClassPathXmlApplicationContext)app).close();
+    }
+
+    @Test
+    //3种构造方法
+    //无参构造方法实例化
+    //工厂静态方法实例化
+    //动态工厂方法实例化
+    public void test3(){
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDaoImpl userDao = (UserDaoImpl) app.getBean("userDao");
+        userDao.save();
     }
 }
