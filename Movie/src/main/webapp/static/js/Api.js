336c322ebe9004b87e08c22ee01fdc197ff8ca4c
DAZHA <47051206+DAZHAdazha@users.noreply.github.com>
2021-03-09 14:36:26 +0800

    Spring + Spring MVC + Mybatis

---
+++ Movie/src/main/webapp/static/js/Api.js
@@ -0,0 +1,8 @@
+// var urlHead = "http:///edy45u.";
+var urlHead = "http://localhost:8080/";
+
+// var urlFoot = "natappfree.cc";
+// var urlFoot = "natappfree.cc";
+
+// var url = urlHead.toString() + urlFoot.toString();
+var url = urlHead.toString() ;
